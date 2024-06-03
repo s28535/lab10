ff470a478a08f243315a337a68ac7fb0c4075961
s28535 <s28535@pjwstk.edu.pl>
2024-06-03 12:41:51 +0200

    final

--- poprawne/WebApplication1/WebApplication1/Entities/Configs/PrescriptionEfConfig.cs
+++ poprawne/WebApplication1/WebApplication1/Entities/Configs/PrescriptionEfConfig.cs
@@ -7,6 +7,24 @@ public class PrescriptionEfConfig : IEntityTypeConfiguration<Prescription>
 {
     public void Configure(EntityTypeBuilder<Prescription> builder)
     {
-        builder.HasKey()
+        builder.HasKey(e => e.IdPrescription).HasName("Prescription_pk");
+
+        builder.Property(e => e.Date).IsRequired().HasDefaultValueSql("GETDATE()");
+
+        builder.Property(e => e.DueDate).IsRequired().HasDefaultValueSql("GETDATE()");
+
+        builder.HasOne(e => e.Patient)
+            .WithMany(e => e.Prescriptions)
+            .HasForeignKey(e => e.IdPatient)
+            .HasConstraintName("Prescription_Patient")
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        builder.HasOne(e => e.Doctor)
+            .WithMany(e => e.Prescriptions)
+            .HasForeignKey(e => e.IdDoctor)
+            .HasConstraintName("Prescription_Doctor")
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        builder.ToTable("Prescription");
     }
 }
