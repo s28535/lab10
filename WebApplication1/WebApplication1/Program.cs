a6c61b680340cf9e0a15b954113d20f7c3977c79
mkostana <milosz.kostana@gmail.com>
2024-06-03 23:00:43 +0200

    final project

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Entities;
+using WebApplication1.Repositories;
+using WebApplication1.Services;
 
 namespace WebApplication1
 {
@@ -15,6 +17,11 @@ namespace WebApplication1
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            
+            builder.Services.AddTransient<IPrescriptionRepository, PrescriptionRepository>();
+            builder.Services.AddTransient<IMedicamentRepository, MedicamentRepository>();
+            builder.Services.AddTransient<IPatientRepository, PatientRepository>();
+            builder.Services.AddTransient<IPrescriptionService, PrescriptionService>();
 
             builder.Services.AddDbContext<HospitalDbContext>(opt =>
             {
